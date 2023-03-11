64330e5ce39eeec4f1ad34b132b560858f345a89
Iris <iridian95@live.com>
2023-03-11 14:51:18 -0500

    adding reaction model

---
+++ models/Reaction.js
@@ -0,0 +1,36 @@
+const { Schema, Types } = require('mongoose');
+const format_date = require('../utils/format_date');
+
+// Schema to create a reaction Schema 
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: date => format_date(date)
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+
+
+module.exports = reactionSchema;
