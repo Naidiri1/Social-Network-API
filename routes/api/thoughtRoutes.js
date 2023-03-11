085fe824c86a3dfd9669fcc4a41a23f0a60c645c
Iris <iridian95@live.com>
2023-03-11 14:41:07 -0500

    adding files

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction
+  
+} = require('../../controllers/thoughtController.js');
+
+// /api/thoughts
+router.route('/').get(getThoughts).post(createThought);
+
+// /api/thoughts/:thoughtId
+router
+  .route('/:thoughtId')
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought)
+  .post(addReaction);
+
+
+  // remove reaction
+  router
+  .route('/:thoughtId/:reactionId')
+  .delete(removeReaction);
+module.exports = router;
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  addFriend,
+  removeFriend,
+  deleteUser,
+  updateUser
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId').get(getSingleUser).delete(deleteUser).put(updateUser);
+
+// /api/users/:userId/friends
+// router.route('/:userId/friends');
+
+// /api/students/:userId/friends/:friendsId
+router.route('/:userId/friends/:friendId').delete(removeFriend).post(addFriend);
+
+
+module.exports = router;
